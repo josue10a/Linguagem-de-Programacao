bb501f5aca06104a1056bf6020b11500c45e738d
Josué <josue1catunda@gmail.com>
2024-10-09 17:06:31 -0300

    mudanças

---
+++ 1 bimestre/atividades/atividade_01.py
@@ -0,0 +1,9 @@
+nome=input("Digite o seu nome: ")
+
+matricula=input("Digite o seu número de matricula: ")
+
+curso=input("Digite o seu curso matriculado: ")
+
+print("\n--- Dados do Aluno ---\n")
+
+print(f" Nome: {nome}\n Número de matrícula: {matricula}\n Curso: {curso}")
